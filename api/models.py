5c9de07c128ec95c845a62f1d041e7b165d48f30
superbottlegod <superbottlegod@naver.com>
2021-10-15 18:31:32 +0900

    Modify model

--- api/models.py
+++ api/models.py
@@ -5,8 +5,16 @@ from django.db.models.base import Model
 class User(models.Model):
     name = models.CharField(max_length=10)
     age = models.IntegerField()
+    weight = models.IntegerField()
+    gender = models.CharField()
 
 class Image(models.Model):
+    user = models.ForeignKey("User", related_name="Image", on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
     image_data = models.ImageField(upload_to="")
 
+class Calendar(models.Model):
+    user = models.ForeignKey("User", related_name="Calendar", on_delete=models.CASCADE)
+
+class Sport(models.Model):
+    pass
