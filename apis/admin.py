481157358c6d7fd4aec3053e4ff91eb4f353bc59
superbottlegod <superbottlegod@naver.com>
2021-10-21 23:46:55 +0900

    10/21 - 로그인 로직 구현

--- apis/admin.py
+++ apis/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from . import models
 from .models import User
-# Register your models here.
 
+# Register your models here.
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
 
     list_display = (
         "username",
+        "userid",
+        "userpassword",
+        "age",
+        "weight",
+        "gender",
     )
 
     # list_display = [field.name for field in User._meta.get_fields()]
--- apis/views.py
+++ apis/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import User
 # Create your views here.
 
 @api_view(['GET'])
@@ -29,8 +29,28 @@ def getRoutes(request):
 def login(request):
     # GET 요청 처리 -> 
     if request.method == 'GET':
-        data = request.data
+        # data = request.data
+        pass
 
     # POST 요청 처리
     if request.method == 'POST':
+        data = request.data
         pass
+    
+    # 로그인 함수 로직 -> 위의 GET , POST 요청 고민해서 로직 넣어야함!
+    input_id = data["userid"]
+    input_password = data["userpassword"]
+    user = User.objects.filter(userid = input_id, userpassword = input_password)
+    user_2 = User.objects.filter(userid = input_id)
+    if not user and user_2:
+        # 아이디는 존재하나, 비밀번호 오류
+        print("비번 오류")
+    elif not user and not user_2:
+        # 로그인 실패
+        print("로그인 실패")
+    else:
+        # 로그인 성공
+        print("Success")
+    
+    # Response 응답 고민!
+    return Response("응답을 뭘 줄껀지도 고민")
