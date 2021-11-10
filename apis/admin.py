1de1920bc1dc460ce2b9f0c1286e03b54dabdb6d
wnsgud0428 <wnsgud0428@naver.com>
2021-11-10 17:13:04 +0900

    fix: apis 앱에 있던 모델들 삭제 -> 별도의 앱(users, exercises)으로 분리 시키자!

--- apis/admin.py
+++ apis/admin.py
@@ -1,26 +1,3 @@
 from django.contrib import admin
-from . import models
 
 # Register your models here.
-@admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-
-    list_display = (
-        "username",
-        "userid",
-        "userpassword",
-        "age",
-        "weight",
-        "gender",
-    )
-    
-
-@admin.register(models.Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-
-    list_display = ()
-
-@admin.register(models.Photo)
-class PhotoAdmin(admin.ModelAdmin):
-
-    list_display = ()
--- apis/models.py
+++ apis/models.py
@@ -1,29 +1,3 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
-# 유저 모델 정의
-class User(models.Model):
-    username = models.CharField(max_length=10)
-    userid = models.CharField(max_length=20, default="")
-    userpassword = models.CharField(max_length=20, default="")
-    age = models.IntegerField(default="0", validators=[MinValueValidator(0), MaxValueValidator(99)])
-    weight = models.IntegerField(default="0")
-    gender = models.TextField(default="Not Selected Yet")
-
-# 운동 피드백 모델 정의
-class Feedback(models.Model):
-    user = models.ForeignKey("User", related_name="feedbacks", on_delete=models.CASCADE, default=False)
-    categori = models.CharField(max_length=10, default="")
-    # day?
-    count = models.IntegerField(default="0")
-    score = models.IntegerField(default="0", validators=[MinValueValidator(0), MaxValueValidator(100)])
-
-# 운동 사진 정의
-class Photo(models.Model):
-    caption = models.CharField(max_length=80)
-    # /photos 폴더에 저장
-    file = models.ImageField(upload_to="photos")
-    feedback = models.ForeignKey("Feedback", related_name="photos", on_delete=models.CASCADE)
-    pass
