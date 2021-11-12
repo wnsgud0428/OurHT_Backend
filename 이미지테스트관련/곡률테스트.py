cf819ff061a5d2f546717570215ae29e89b974c8
superbottlegod <superbottlegod@naver.com>
2021-11-12 17:16:32 +0900

    11/12 - 등 곡률 알고리즘 수정 중

--- 이미지테스트관련/곡률테스트.py
+++ 이미지테스트관련/곡률테스트.py
@@ -1,8 +1,87 @@
 import matplotlib.pyplot as plt
-import matplotlib 
+import math, cv2, numpy as np
 
-test_list = [[4, 2], [5, 1], [6, 3], [7, 11]]
-print(list(zip(*test_list))[0])
-plt.scatter(list(zip(*test_list))[0], list(zip(*test_list))[1])
+# 임의의 두 점이 주어질 때, 직선의 기울기 찾기
+def find_straightslope(first_x, first_y, second_x, second_y):
 
+    # 기울기 구하기
+    # 기울기 0과 무한대인경우 예외처리
+    if first_x == second_x:
+        slope = 9999999
+    elif first_y == second_y:
+        slope = -9999999
+    else:
+        slope = (second_y - first_y) / (second_x - first_x)
+    return slope
+
+# 점과 직선사이 거리 찾기
+def find_distancefromline(slope, line_x, line_y, point_x, point_y):
+
+    # 기울기 0과 무한대인경우 예외처리
+    if slope == 9999999:
+        return abs(point_x - line_x)
+    elif slope == -9999999:
+        return abs(point_y - line_y)
+
+    # 연립방정식 두개 풀기
+    left_side = np.array([[slope, -1], [-1/slope, -1]])
+    right_side = np.array([slope * line_x - line_y, (-1/slope) * point_x - point_y])
+    result = np.linalg.solve(left_side, right_side)
+
+    # 교점과 주어진 점 거리 구하기
+    dist_x, dist_y = pow(result[0] - point_x, 2), pow(result[1] - point_y, 2)
+    answer = math.sqrt(dist_x + dist_y)
+    
+    return answer
+
+# print(find_distancefromline())
+
+# 한 직선이 주어지고, 그 직선에 수직인 직선을 따라가면서 그림의 경계점까지의 거리 찾기
+def find_distancefromboarderline(picture, slope, start_x, start_y, end_x):
+
+    # 그림의 가로, 세로 크기
+    len_y = picture.shape[0]  # 세로
+    len_x = picture.shape[1]  # 가로
+    print(len_y, len_x)
+    # 길이, 좌표를 담은 배열
+    distance_result, coor_result = [], []
+
+    # 주어진 직선 정의
+    basic_line = [slope, (-slope * start_x) + start_y]
+
+    # 수직인 직선 정의
+    new_slope = -1/slope
+    perpen_line = [new_slope, (-new_slope * start_x) + start_y]
+
+    slope_step = 1
+    for i in range(start_x, end_x, slope_step):
+        new_x, new_y = i, (perpen_line[0] * i) + perpen_line[1]
+        new_y = int(round(new_y))
+        if new_y < 0:
+            break
+        while picture[len_y - new_y - 1][new_x][0] != 0 or picture[len_y - new_y - 1][new_x][1] != 0 or picture[len_y - new_y - 1][new_x][2] != 0:
+            # 직선 따라가면서 경계 찾기
+            if new_slope > 0:
+                new_x += slope_step
+            else:
+                new_x -= slope_step
+            new_y = (perpen_line[0] * new_x) + perpen_line[1]
+            new_y = int(round(new_y))
+            if (new_x >= len_x) or (new_x < 0) or (new_y >= len_y) or (new_y < 0):
+                break
+        print(i, basic_line[0] * i + basic_line[1], new_x, new_y)
+        distance_result.append([i,math.sqrt((i - new_x) * (i - new_x) + (basic_line[0] * i + basic_line[1] - new_y) * (basic_line[0] * i + basic_line[1] - new_y))])
+        coor_result.append([new_x, new_y])
+
+    return distance_result, coor_result
+
+
+testimage = cv2.imread("testimage.png", 1)
+testimage = np.array(testimage)
+print(testimage[341 - 94 - 1][500])
+test_slope = find_straightslope(255, 100, 383, 188)
+test_arr, coor_arr = find_distancefromboarderline(testimage, test_slope, 255, 100, 383)
+#print(test_arr, coor_arr)
+plt.scatter(list(zip(*test_arr))[0], list(zip(*test_arr))[1])
+plt.scatter(list(zip(*coor_arr))[0], list(zip(*coor_arr))[1])
 plt.show()
