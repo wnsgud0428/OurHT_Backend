71337b4acbf070cb83672a3d27a098b694dfb620
wnsgud0428 <wnsgud0428@naver.com>
2021-11-18 15:27:24 +0900

    fix: 상체각도 체크함수, 고개 각도체크함수 정교화

--- apis/pose_feedback.py
+++ apis/pose_feedback.py
@@ -1,49 +1,4 @@
-import numpy as np
-
-
-"""
-0: nose
-1: left_eye
-2: right_eye
-3: left_ear
-4: right_ear
-5: left_shoulder
-6: right_shoulder
-7: left_elbow
-8: right_elbow
-9: left_wrist
-10: right_wrist
-11: left_hip
-12: right_hip
-13: left_knee
-14: right_knee
-15: left_ankle
-16: right_ankle
-"""
-
-# 각도 계산 하는 함수 a-b-c의 각도
-"""
-b----a
- `
-  `
-   `c
-"""
-
-
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
-
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
-        a[1] - b[1], a[0] - b[0]
-    )
-    angle = np.abs(radians * 180.0 / np.pi)
-
-    if angle > 180.0:
-        angle = 360 - angle
-
-    return angle
+from util import calculate_angle
 
 
 def isUpperbodyNotBent(data):
@@ -51,23 +6,36 @@ def isUpperbodyNotBent(data):
 
     left_shoulder_x = data["keypoints"][5]["position"]["x"]
     left_shoulder_y = data["keypoints"][5]["position"]["y"]
+    right_shoulder_x = data["keypoints"][6]["position"]["x"]
+    right_shoulder_y = data["keypoints"][6]["position"]["y"]
 
     left_hip_x = data["keypoints"][11]["position"]["x"]
     left_hip_y = data["keypoints"][11]["position"]["y"]
+    right_hip_x = data["keypoints"][12]["position"]["x"]
+    right_hip_y = data["keypoints"][12]["position"]["y"]
 
     left_knee_x = data["keypoints"][13]["position"]["x"]
     left_knee_y = data["keypoints"][13]["position"]["y"]
+    right_knee_x = data["keypoints"][14]["position"]["x"]
+    right_knee_y = data["keypoints"][14]["position"]["y"]
 
-    left_ankle_x = data["keypoints"][15]["position"]["x"]
-    left_ankle_y = data["keypoints"][15]["position"]["y"]
+    # left_ankle_x = data["keypoints"][15]["position"]["x"]
+    # left_ankle_y = data["keypoints"][15]["position"]["y"]
 
-    left_shoulder = [left_shoulder_x, left_shoulder_y]
-    left_hip = [left_hip_x, left_hip_y]
-    left_knee = [left_knee_x, left_knee_y]
-    left_ankle = [left_ankle_x, left_ankle_y]
+    shoulder_x = (left_shoulder_x + right_shoulder_x) / 2
+    shoulder_y = (left_shoulder_y + right_shoulder_y) / 2
+    hip_x = (left_hip_x + right_hip_x) / 2
+    hip_y = (left_hip_y + right_hip_y) / 2
+    knee_x = (left_knee_x + right_knee_x) / 2
+    knee_y = (left_knee_y + right_knee_y) / 2
 
-    shoulder_hip_knee_angle = calculate_angle(left_shoulder, left_hip, left_knee)
-    hip_knee_ankle_angle = calculate_angle(left_hip, left_knee, left_ankle)
+    shoulder = [shoulder_x, shoulder_y]
+    hip = [hip_x, hip_y]
+    knee = [knee_x, knee_y]
+    # left_ankle = [left_ankle_x, left_ankle_y]
+
+    shoulder_hip_knee_angle = calculate_angle(shoulder, hip, knee)
+    # hip_knee_ankle_angle = calculate_angle(left_hip, left_knee, left_ankle)
 
     ### 발목의 중앙 정렬을 위해 --> 정확도 향상을 하려면 left_ankle뿐만 아니라 right_ankle도 고려해야됨
     # print(f"어깨-엉덩이-무릎 각도:{shoulder_hip_knee_angle}")
@@ -93,36 +61,46 @@ def isUpperbodyNotBent(data):
 def isFaceForward(data):
     is_face_forward = False
 
-    left_eye_x = data["keypoints"][1]["position"]["x"]
-    left_eye_y = data["keypoints"][1]["position"]["y"]
+    nose_x = data["keypoints"][0]["position"]["x"]
+    nose_y = data["keypoints"][0]["position"]["y"]
 
     left_ear_x = data["keypoints"][3]["position"]["x"]
     left_ear_y = data["keypoints"][3]["position"]["y"]
+    right_ear_x = data["keypoints"][4]["position"]["x"]
+    right_ear_y = data["keypoints"][4]["position"]["y"]
 
-    left_shoulder_x = data["keypoints"][5]["position"]["x"]
-    left_shoulder_y = data["keypoints"][5]["position"]["y"]
-
-    left_eye = [left_eye_x, left_eye_y]
+    nose = [nose_x, nose_y]
     left_ear = [left_ear_x, left_ear_y]
-    left_shoulder = [left_shoulder_x, left_shoulder_y]
+    right_ear = [right_ear_x, right_ear_y]
+    extra_point = []
+    ear_nose_extra_angle = 0
 
-    eye_ear_shoulder_angle = calculate_angle(left_eye, left_ear, left_shoulder)
+    if returnPersonFaceDirection(data) == "right face is front":
+        print("right face is front")  # test
+        extra_point = [right_ear_x, nose_y]
+        ear_nose_extra_angle = calculate_angle(right_ear, nose, extra_point)
+    elif returnPersonFaceDirection(data) == "left face is front":
+        print("left face is front")  # test
+        extra_point = [left_ear_x, nose_y]
+        ear_nose_extra_angle = calculate_angle(right_ear, nose, extra_point)
 
-    ###스쿼트시 고개를 잘 유지하는지
-    # print(f"눈-귀-어깨 각도:{eye_ear_shoulder_angle}")
-
-    print(eye_ear_shoulder_angle)
-    if 70 < eye_ear_shoulder_angle < 90:
+    print(ear_nose_extra_angle)  # test
+    if ear_nose_extra_angle < 20:  # todo: 값 수정
         is_face_forward = True
-        print("2-4: 적당히 고개를 들었어요")
-    elif eye_ear_shoulder_angle >= 90:
-        is_face_forward = False
-        print("고개를 너무 들었어요!")
-    elif eye_ear_shoulder_angle <= 70:
-        is_face_forward = None
-        print("고개를 너무 숙였어요!")
 
     if is_face_forward == True:
         return True
     else:
         return False
+
+
+def returnPersonFaceDirection(data):
+    """사람이 어느쪽으로 서있는지 return함"""
+    left_eye_x = data["keypoints"][1]["position"]["x"]
+    right_eye_x = data["keypoints"][2]["position"]["x"]
+    nose_x = data["keypoints"][0]["position"]["x"]
+
+    if left_eye_x < nose_x and right_eye_x < nose_x:
+        return "right face is front"
+    else:
+        return "left face is front"
