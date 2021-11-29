a5fb5402bf06d15d342a372c76961c2670d044b2
superbottlegod <superbottlegod@naver.com>
2021-11-29 16:02:00 +0900

    11/29 - 로그인

--- apis/views.py
+++ apis/views.py
@@ -1,6 +1,7 @@
 import cv2, numpy, util, base64
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from users import serializer as user_serializer
 from exercises import serializer as exercise_serializer
@@ -32,37 +33,14 @@ def getRoutes(request):
     return Response(routes)
 
 
-# 'apis/users/login' - 유저 로그인 기능 처리 함수
-@api_view(["GET", "POST"])
+# 'apis/users/login' - 토큰 통해 유저 로그인 기능 처리 함수
+@api_view(["POST"])
 def login(request):
-    # GET 요청 처리 ->
-    if request.method == "GET":
-        # data = request.data
-        pass
-
-    # POST 요청 처리
     if request.method == "POST":
-        data = request.data
-        pass
-
-    # 로그인 함수 로직 -> 위의 GET , POST 요청 고민해서 로직 넣어야함!
-    input_id = data["userid"]
-    input_password = data["userpassword"]
-    user = user_models.User.objects.filter(userid=input_id, userpassword=input_password)
-    user_2 = user_models.User.objects.filter(userid=input_id)
-    if not user and user_2:
-        # 아이디는 존재하나, 비밀번호 오류
-        print("비번 오류")
-    elif not user and not user_2:
-        # 로그인 실패
-        print("로그인 실패")
-    else:
-        # 로그인 성공
-        print("Success")
-
-    # Response 응답 고민!
-    return Response("응답을 뭘 줄껀지도 고민")
-
+        usertoken = request.data["usertoken"]
+        print(usertoken)
+        token = Token.objects.get(key=usertoken)
+        return Response(token.user.id)
 
 # 'apis/users/createexercise' - Exercise 모델 생성, 생성된 모델의 PK를 응답함
 @api_view(["POST"])
--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html")),
     path("apis/", include("apis.urls")),
+    path("rest-auth/", include('rest_auth.urls')),
 ]
