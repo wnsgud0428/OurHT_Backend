8abf6591ad3dab920f98bdf2f66c890907dc99d9
wnsgud0428 <wnsgud0428@naver.com>
2021-12-10 00:37:58 +0900

    .

--- exercises/serializer.py
+++ exercises/serializer.py
@@ -1,23 +1,24 @@
 from django.db.models.fields import CharField
-from numpy.lib.utils import source
 from rest_framework.serializers import ModelSerializer
 from apis import serializer
-from apis.feedback import checkKneeposition
 from exercises import models as exercise_models
+
 
 class ExerciseSerializer(ModelSerializer):
     class Meta:
         model = exercise_models.Exercise
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ChecklistSerializer(ModelSerializer):
     class Meta:
         model = exercise_models.Checklist
-        fields = ('pk',)
+        fields = ("pk",)
+
 
 class MotionSerializer(ModelSerializer):
     checklist = ChecklistSerializer(read_only=True, many=True)
+
     class Meta:
         model = exercise_models.Motion
-        fields = '__all__'
-
+        fields = "__all__"
